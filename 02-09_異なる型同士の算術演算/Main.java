3444013d145dca20d3f1cb0d80818c5831fd2d06
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-11-16 18:10:32 +0900

    02-09_異なる型同士の算術演算

---
+++ 02-09_異なる型同士の算術演算/Main.java
@@ -0,0 +1,10 @@
+//異なる型同士の算術演算
+public class Main {
+	public static void main(String[]args) {
+		double d=8.5/2; //intからdoubleへ型変換
+		long l=5+2L; //intからlongへ型変換
+		System.out.println(d);
+		System.out.println(l);
+	}
+
+}
