db26111387ef31a1f18b1b24976de76356cf3939
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-11-17 17:35:06 +0900

    02-12_改行なし命令実行の文

---
+++ 02-12_改行なし命令実行の文/Main.java
@@ -0,0 +1,9 @@
+//改行なし画面出力
+public class Main {
+	public static void main(String[]args) {
+		String name="神酒まめ";
+		System.out.print("私の名前は");
+		System.out.print(name);
+		System.out.print("です");
+	}
+}
