354b1ca2850b89f2772486840632df877a2eee6c
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-11-18 17:18:16 +0900

    03-02_トイレの空きを待つ繰り返し

---
+++ 03-02_トイレの空きを待つ繰り返し/Main.java
@@ -0,0 +1,11 @@
+//トイレの空きを待つ繰り返し
+public class Main {
+	public static void main(String[]args) {
+		boolean doorClose=true;
+		while(doorClose==true) {
+			System.out.println("ノックする");
+			System.out.println("1分待つ");
+		}
+	}
+
+}
