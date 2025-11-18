8589124c1ecac32f353a53d6df8c32f73f85d497
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-11-18 17:13:11 +0900

    03-01_天気による行動の変化をJavaで表す

---
+++ 03-01_天気による行動の変化をJavaで表す/Main.java
@@ -0,0 +1,13 @@
+//てんきによる行動の変化をJavaで表す
+public class Main {
+	public static void main(String[]args) {
+		boolean tenki=false;
+		if(tenki==true) {
+			System.out.println("洗濯をします");
+			System.out.println("散歩に行きます");
+		}else {
+			System.out.println("映画を見ます");
+		}
+	}
+
+}
