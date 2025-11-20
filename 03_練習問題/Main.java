c25d0b60b4979d89ca1c81147382f674e5e50ac4
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-11-20 16:54:55 +0900

    03_練習問題

---
+++ 03_練習問題/Main.java
@@ -0,0 +1,19 @@
+//3章練習問題・数当てゲーム
+public class Main {
+	public static void main(String[]args) {
+		System.out.println("【数あてゲーム】");
+		int ans=new java.util.Random().nextInt(10);
+		for(int i=0;i<5;i++) {
+			System.out.println("０～９の数字を入力してください");
+			int num = new java.util.Scanner(System.in).nextInt();
+			if(ans==num) {
+				System.out.println("あたり");
+				break;
+			}else {
+				System.out.println("ちがいます");
+			}
+		}
+		System.out.println("ゲームを終了します");
+	}
+
+}
