6ac5f0c26424e22dbbf9d4ec0fe93a71bd4be8b9
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-11-20 17:55:54 +0900

    04-12_DNAの記号をランダムに表示する

---
+++ 04-12_DNAの記号をランダムに表示する/Main.java
@@ -0,0 +1,15 @@
+//DNAの記号をランダムに表示する
+public class Main {
+	public static void main(String[]args) {
+		int[]seq=new int[10];
+		
+		for(int i=0;i<seq.length;i++) {
+			seq[i]=new java.util.Random().nextInt(4);
+		}
+		
+		for(int i=0;i<seq.length;i++) {
+			char[] base = {'A','T','G','C'};
+			System.out.println(base[seq[i]]+"");
+		}
+	}
+}
