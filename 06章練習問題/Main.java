bf9748c3593ea50bdfb41e773fd24e1f8c253542
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-11-22 21:16:27 +0900

    06章練習問題

---
+++ 06章練習問題/Main.java
@@ -0,0 +1,13 @@
+//第６章練習問題
+
+import comment.Zenhan;
+
+public class Main {
+	public static void main(String[]args)throws Exception{
+		Zenhan.doWarusa();
+		Zenhan.doTogame();
+		comment.Kouhan.callDeae();
+		comment.Kouhan.showMondokoro();
+	}
+
+}
