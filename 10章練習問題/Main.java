d4b65493ba05679083fa238cc2092088e2f2d972
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-11-25 19:42:03 +0900

    10章練習問題

---
+++ 10章練習問題/Main.java
@@ -0,0 +1,15 @@
+//10章練習問題
+//クラスに対する親クラス→子クラスを考え自由に列挙せよ
+//（例:孫→子→親）SuperHero → Hero → Character
+//MobilePhone → Phone → Device
+//SuperCar → Car → Property
+//Encycropedia → Dictionary → Book
+public class Main {
+	public static void main(String[]args) {
+		System.out.println("クラスに対する親クラス→子クラスを考え自由に列挙せよ");
+		System.out.println("（例:孫→子→親）SuperHero → Hero → Character");
+		System.out.println("MobilePhone → Phone → Device");
+		System.out.println("SuperCar → Car → Property");
+	}
+
+}
