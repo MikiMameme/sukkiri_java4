0cc7f9756d92f21be795b84388c2c19d7ed3a5e3
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-11-27 19:11:24 +0900

    12章練習問題

---
+++ 12章練習問題/A.java
@@ -0,0 +1,13 @@
+//12章練習問題（詳細はテキスト参照）
+//インスタンス生成、変数に応じて呼べるメソッドを全て挙げよ
+//Sword, Item, Sword, Item
+//AとBのインスタンスを生成、実行した場合、表示される内容を答えよ
+//Monster, Slime, Slime, Monster
+public class A {
+	public void main(String[]args) {
+		System.out.println("インスタンス生成、変数に応じて呼べるメソッドを全て挙げよ");
+		System.out.println("Sword, Item, Sword, Item");
+		System.out.println("AとBのインスタンスを生成、実行した場合、表示される内容を答えよ");
+		System.out.println("Monster, Slime, Slime, Monster");
+	}
+}
