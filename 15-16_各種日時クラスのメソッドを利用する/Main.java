5704177c8221ac3a33e2a00beb0a83352b80e5e1
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-11-30 20:12:05 +0900

    15-16_各種日時クラスのメソッドを利用する

---
+++ 15-16_各種日時クラスのメソッドを利用する/Main.java
@@ -0,0 +1,18 @@
+import java.time.*;
+import java.time.format.*;
+
+public class Main {
+	public static void main(String[]args) {
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+		LocalDate ldate = LocalDate.parse("2023/09/22",fmt);
+		
+		LocalDate ldatep = ldate.plusDays(1000);
+		String str = ldatep.format(fmt);
+		System.out.println("1000日後は"+str);
+		
+		LocalDate now = LocalDate.now();
+		if(now.isAfter(ldatep)) {
+			System.out.println("本日は、その日より未来です");
+		}
+	}
+}
