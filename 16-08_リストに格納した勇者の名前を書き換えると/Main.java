90a30988c5c755764d2250e0e4c3e028b1e85f9e
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-12-02 19:28:29 +0900

    16-08_リストに格納した勇者の名前を書き換えると

---
+++ 16-08_リストに格納した勇者の名前を書き換えると/Main.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Hero {
+	public String name;
+}
+
+public class Main{
+	public static void main(String[]args) {
+		Hero h = new Hero();
+		h.name = "ヨシヒコ";
+		List<Hero> list = new ArrayList<Hero>();
+		list.add(h);
+		h.name = "ダンジョー";
+		System.out.println(list.get(0).name);
+	}
+}
