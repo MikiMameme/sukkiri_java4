3c32d3a27fa6127608d29fd2b2eafcaa8fcbbad5
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-12-02 19:28:15 +0900

    16-02_イテレータを使った繰り返し処理

---
+++ 16-02_イテレータを使った繰り返し処理/Main.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Main {
+	public static void main(String[]args) {
+		ArrayList<String>names = new ArrayList<String>();
+		names.add("フシギダネ");
+		names.add("ヒトカゲ");
+		names.add("ゼニガメ");
+		Iterator<String> it = names.iterator();
+		while(it.hasNext()) {
+			String e = it.next();
+			System.out.println(e);
+		}
+	}
+
+}
