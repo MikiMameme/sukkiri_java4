b49d4d1e74d40d5fce2ea3c0419819aec5581b3a
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-12-03 20:58:43 +0900

    17-11_例外インスタンスを自分で投げる

---
+++ 17-11_例外インスタンスを自分で投げる/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+	public static void main(String[]args) {
+		Person p = new Person();
+		
+		//わざと変な値を設定してみる
+		p.setAge(-128);
+	}
+
+}
---
+++ 17-11_例外インスタンスを自分で投げる/Person.java
@@ -0,0 +1,11 @@
+
+public class Person {
+	int age;
+	public void setAge(int age) {
+		if(age < 0) {
+			throw new IllegalArgumentException("年齢は０以上の値を指定してください。指定値=" + age);
+		}
+		this.age = age;
+	}
+
+}
