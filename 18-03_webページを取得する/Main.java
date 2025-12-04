2887be4eb5f3641705b1deeb5ed95a107dc27d9c
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-12-04 18:47:38 +0900

    18-03_webページを取得する

---
+++ 18-03_webページを取得する/Main.java
@@ -0,0 +1,16 @@
+import java.io.InputStream;
+import java.net.URL;
+
+public class Main {
+	public static void main(String[]args)throws Exception{
+		URL u = new URL("https://www.google.com/");
+		InputStream is = u.openStream();
+		int i = is.read();
+		while(i != -1) {
+			char c = (char)i;
+			System.out.print(c);
+			i = is.read();
+		}
+	}
+
+}
